92efc6e28f6fa2ceb23fdc4959f84610d559e7f5
kobedeutscher <kobe.deutscher@gmail.com>
2024-11-21 13:15:33 -0800

    testing for dart

--- chitchat_app/lib/query_attempts.dart
+++ chitchat_app/lib/query_attempts.dart
@@ -6,16 +6,18 @@ String usersCookie = "";
 int globalUserId = -1;
 
 void main(List<String> arguments) async {
-  // This example uses the Google Books API to search for books about http.
-  // https://developers.google.com/books/docs/overview
-
-  await loginAttempt("test@localhost.lan", "password");
-  await addPostToDatabase("This is a example post", 1);
+  // Example usage:
+  await retrieveUsername(1);
+  await addPostToDatabase("This is an example post", 1);
+  await usernameAndContentDataAllPosts();
 }
-//Done!!!
-Future<String> retrieveUsername(int userId) async {
+
+// retrieveUsername function
+Future<String> retrieveUsername(int userId, {http.Client? client}) async {
+  client ??= http.Client(); // Use the provided client or create a new one
   var url = getUsername(userId);
-  var response = await http.get(url);
+  var response = await client.get(url);
+
   if (response.statusCode == 200) {
     Map<String, dynamic> jsonMap = jsonDecode(response.body);
     return jsonMap['name'];
@@ -24,26 +26,26 @@ Future<String> retrieveUsername(int userId) async {
   }
 }
 
-//Done!!!
-Future<List<List<String>>> usernameAndContentDataAllPosts() async {
+// usernameAndContentDataAllPosts function
+Future<List<List<String>>> usernameAndContentDataAllPosts({http.Client? client}) async {
   List<List<String>> returnedThing = [];
   Map<String, String> idToUsername = {};
 
+  client ??= http.Client(); // Use the provided client or create a new one
   var url = getAllPosts();
-  var response = await http.get(url);
+  var response = await client.get(url);
 
   if (response.statusCode == 200) {
+    print(response.body);
     String stringJsonResponse = response.body;
-    stringJsonResponse = stringJsonResponse.substring(2, stringJsonResponse.length-2);
+    stringJsonResponse = stringJsonResponse.substring(2, stringJsonResponse.length - 2);
     List<String> listJson = stringJsonResponse.split('\', \'');
-    
+
     List<Map<String, dynamic>> posts = [];
-  
     for (var jsonStr in listJson) {
-    // Decode each JSON string into a Map and add to the list
       posts.add(jsonDecode(jsonStr));
     }
-    
+
     int numIterations = 0;
     for (var post in posts) {
       returnedThing.add([]);
@@ -52,45 +54,43 @@ Future<List<List<String>>> usernameAndContentDataAllPosts() async {
       String content = post['contents'].toString();
       String postId = post['id'].toString();
 
-      //looks through local list of list to see if we already have to see if we have the username
       if (idToUsername.containsKey(userId)) {
         username = idToUsername[userId]!;
-      } else { //wamp wamp :(
-        username = await retrieveUsername(int.parse(userId));
+      } else {
+        username = await retrieveUsername(int.parse(userId), client: client);
         idToUsername[userId] = username;
       }
 
-      returnedThing[numIterations].add(username); 
+      returnedThing[numIterations].add(username);
       returnedThing[numIterations].add(content);
       returnedThing[numIterations].add(postId);
       numIterations++;
     }
     return returnedThing;
   } else {
-    return ([["UserUnknown","failed to get posts"]]);
+    return [["UserUnknown", "failed to get posts"]];
   }
 }
 
-//Done!!!
-Future<List<List<String>>> usernameAndContentDataAllComments(int postId) async {
+// usernameAndContentDataAllComments function
+Future<List<List<String>>> usernameAndContentDataAllComments(int postId, {http.Client? client}) async {
   List<List<String>> returnedThing = [];
   Map<String, String> idToUsername = {};
 
+  client ??= http.Client(); // Use the provided client or create a new one
   var url = getAllComments(postId);
-  var response = await http.get(url);
+  var response = await client.get(url);
 
   if (response.statusCode == 200) {
     String stringJsonResponse = response.body;
-    stringJsonResponse = stringJsonResponse.substring(2, stringJsonResponse.length-2);
+    stringJsonResponse = stringJsonResponse.substring(2, stringJsonResponse.length - 2);
     List<String> listJson = stringJsonResponse.split('\', \'');
-    
+
     List<Map<String, dynamic>> posts = [];
-  
     for (var jsonStr in listJson) {
-    // Decode each JSON string into a Map and add to the list
       posts.add(jsonDecode(jsonStr));
     }
-    
+
     int numIterations = 0;
     for (var post in posts) {
       returnedThing.add([]);
@@ -99,127 +99,110 @@ Future<List<List<String>>> usernameAndContentDataAllComments(int postId) async {
       String content = post['contents'].toString();
       String postId = post['id'].toString();
 
-      //looks through local list of list to see if we already have to see if we have the username
       if (idToUsername.containsKey(userId)) {
         username = idToUsername[userId]!;
-      } else { //wamp wamp :(
-        username = await retrieveUsername(int.parse(userId));
+      } else {
+        username = await retrieveUsername(int.parse(userId), client: client);
         idToUsername[userId] = username;
       }
 
-      returnedThing[numIterations].add(username); 
+      returnedThing[numIterations].add(username);
       returnedThing[numIterations].add(content);
       returnedThing[numIterations].add(postId);
       numIterations++;
     }
- 
     return returnedThing;
   } else {
-    return ([["UserUnknown","failed to get posts"]]);
+    return [["UserUnknown", "failed to get posts"]];
   }
 }
 
-//Done!!!
-Future<bool> addPostToDatabase(String postContent, int userid) async {
+// addPostToDatabase function
+Future<bool> addPostToDatabase(String postContent, int userId, {http.Client? client}) async {
+  client ??= http.Client(); // Use the provided client or create a new one
   var uri = createPost();
-  String id = "$userid";
   final cookie = <String, String>{};
   cookie['cookie'] = usersCookie;
   final map = <String, dynamic>{};
   map['content'] = postContent;
-  map['userid'] = id;
-  
+  map['userid'] = "$userId";
 
-  http.Response response = await http.post(
+  http.Response response = await client.post(
     uri,
     headers: cookie,
     body: map,
   );
 
-  if (response.statusCode == 200) {
-    return true;
-  } else {
-    return false;
-  }
+  return response.statusCode == 200;
 }
 
-//Done!!!
-Future<bool> addCommentToDatabase(String postContent, int userid, int postId) async {
+// addCommentToDatabase function
+Future<bool> addCommentToDatabase(String postContent, int userId, int postId, {http.Client? client}) async {
+  client ??= http.Client(); // Use the provided client or create a new one
   var uri = createComment(postId);
-  String id = "$userid";
   final cookie = <String, String>{};
   cookie['cookie'] = usersCookie;
   final map = <String, dynamic>{};
   map['content'] = postContent;
-  map['userid'] = id;
+  map['userid'] = "$userId";
 
-  http.Response response = await http.post(
+  http.Response response = await client.post(
     uri,
     headers: cookie,
     body: map,
   );
 
-  if (response.statusCode == 200) {
-    return true;
-  } else {
-    return false;
-  }
+  return response.statusCode == 200;
 }
 
-//Done!!!
-Future<bool> deletePost(int postId) async {
+// deletePost function
+Future<bool> deletePost(int postId, {http.Client? client}) async {
+  client ??= http.Client(); // Use the provided client or create a new one
   var uri = removePost(postId);
   final map = <String, String>{};
   map['cookie'] = usersCookie;
-  http.Response response = await http.delete(
+  http.Response response = await client.delete(
     uri,
     headers: map,
   );
 
-  if (response.statusCode == 200) {
-    return true;
-  } else {
-    return false;
-  }
+  return response.statusCode == 200;
 }
 
-//Done!!!
-Future<bool> deleteComment(int postId, int commentId) async {
+// deleteComment function
+Future<bool> deleteComment(int postId, int commentId, {http.Client? client}) async {
+  client ??= http.Client(); // Use the provided client or create a new one
   var uri = removeComment(postId, commentId);
   final map = <String, String>{};
   map['cookie'] = usersCookie;
-  http.Response response = await http.delete(
+  http.Response response = await client.delete(
     uri,
     headers: map,
   );
 
-  if (response.statusCode == 200) {
-    return true;
-  } else {
-    return false;
-  }
+  return response.statusCode == 200;
 }
 
-//DONE!!!
-Future<List<bool>> loginAttempt(String username, String password) async {
-  
+// loginAttempt function
+Future<List<bool>> loginAttempt(String username, String password, {http.Client? client}) async {
+  client ??= http.Client(); // Use the provided client or create a new one
   var uri = login();
   final map = <String, dynamic>{};
   map['email'] = username;
   map['password'] = password;
-  http.Response response = await http.post(
+  http.Response response = await client.post(
     uri,
     body: map,
   );
 
   if (response.statusCode == 200) {
-    usersCookie = response.headers['set-cookie'].toString().split(';')[0];
-    String token = usersCookie.toString().split("session=")[1].split(';')[0].split('.')[0];
+    usersCookie = response.headers['set-cookie']!.split(';')[0];
+    String token = usersCookie.split("session=")[1].split(';')[0].split('.')[0];
     Codec<String, String> stringToBase64 = utf8.fuse(base64Url);
-    String data = stringToBase64.decode(token+("="*(token.length%4)));
+    String data = stringToBase64.decode(token + ("=" * (token.length % 4)));
     Map<String, dynamic> jsonMap = jsonDecode(data);
-    
-    jsonMap['user_id:'] = globalUserId;
+
+    globalUserId = jsonMap['user_id:']; // Storing globalUserId
     return [true, jsonMap['is_admin']];
   } else {
     return [false, false];
